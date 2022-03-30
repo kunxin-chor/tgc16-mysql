ab1b9c1a18ba849a590f635dc73864474c8246b7
kunxin-chor <chorkunxin@yahoo.com>
2022-03-30 07:15:00 +0000

    Added subqueries

--- data-query-language.sql
+++ data-query-language.sql
@@ -171,3 +171,36 @@ select YEAR(orderDate), MONTH(orderDate), COUNT(*) from orders join orderdetails
  ON orders.orderNumber = orderdetails.orderNumber
  WHERE YEAR(orderDate) >= 2003 AND YEAR(orderDate) <=2004
  GROUP BY YEAR(orderDate), MONTH(orderDate)
+
+ -- SUB QUERIES
+ -- ex:1 find all customers whose credit limit above the average
+ select * from customers where creditLimit > (select avg(creditLimit) from customers);
+
+-- ex:2 find all products that have not been ordered before
+ select * from products where productCode not in (select distinct(productCode) from orderdetails)
+
+ -- ex:3 use a subquery to find all customers with no sales rep employee number
+ select * from customers where customerNumber NOT IN (
+select customerNumber from customers where salesRepEmployeeNumber IS NOT NULL);
+
+-- ex:4 
+SELECT employees.employeeNumber, employees.firstName, employees.lastName, SUM(amount) FROM employees JOIN customers
+   ON employees.employeeNumber = customers.salesRepEmployeeNumber
+ JOIN payments
+   ON customers.customerNumber = payments.customerNumber
+ GROUP BY employees.employeeNumber, employees.firstName, employees.lastName
+ HAVING sum(amount) > (select sum(amount) * 0.1 from payments);
+
+ -- BONUS: find the best selling product for each year and month:
+ select year(orderDate) as orderYear, month(orderDate) as orderMonth, productCode, sum(quantityOrdered) from orderdetails 
+  join orders on orderdetails.orderNumber = orders.orderNumber
+group by productCode, month(orderDate), year(orderDate)
+having productCode = ( select productCode from orderdetails 
+  join orders on orderdetails.orderNumber = orders.orderNumber
+  where year(orderDate)=orderYear and month(orderDate)=orderMonth
+  group by productCode
+  order by sum(quantityOrdered) desc
+  limit 1
+ )
+ order by year(orderDate), month(orderDate);
+
